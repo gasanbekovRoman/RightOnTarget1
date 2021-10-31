7e5c79efdbf32aafd36a4596e215f244c66cd9a7
GasanbekovRoman <spamerl256@gmail.com>
2021-10-31 22:11:06 +0400

    1

--- RightOnTarget/ViewController.swift
+++ RightOnTarget/ViewController.swift
@@ -8,6 +8,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var slider: UISlider!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
